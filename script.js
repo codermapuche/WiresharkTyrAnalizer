0ed769f1fc302770dfbdce469d958b652f474217
Nehuen Prados <nehuensd@gmail.com>
2017-05-26 21:01:47 -0300

    Update

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ WiresharkTyrAnalizer = function(capture_path) {
 	this.parse_ready = false;
 	this.callbacks = { };
 	this.load_capture_file(capture_path, this.parse);
+  this.traffic = [];
+  this.updateFilter = this.updateFilter.bind(this);
 }
 
 /*/
@@ -16,21 +18,23 @@ WiresharkTyrAnalizer = function(capture_path) {
 /*/
 WiresharkTyrAnalizer.prototype = {
 	"animation_delay" : 4000,
-	"traffic" : [],
 	"i18n" : {
 		"arp" : {
-			"who_has" : "¿Quien tiene esta IP? <strong>{IP}</strong>",
-			"i_has" : "La tengo yo: <em>{MAC}</em>",
+			"who_has" : "¿Quien tiene la IP <strong>{IP}</strong>?",
+			"i_has" : "¡Yo tengo la IP <strong>{IP}</strong>!",
 		},
 		"tcp" : {
-			"syn" : "Quiero abrir una conexion en el sentido: <strong>{SOURCE} → {DESTINATION}</strong>",
-			"syn_ack" : "Acepto tu conexion y quiero abrir una en el sentido: <strong>{SOURCE} → {DESTINATION}</strong> ",
-			"ack" : "Ok",
+			"syn" : "¿Vos me confirmas esta conexion: <strong>{SOURCE} → {DESTINATION}</strong>?",
+			"syn_ack" : "Si confirmado.<br><br>¿Vos me confirmas esta conexion: <strong>{SOURCE} → {DESTINATION}</strong>?",
+			"ack" : "Si confirmado.",
 			"ooo" : "<strong class=\"fail\">¡El paquete llego fuera de orden!</strong>",
 			"retransmission" : "<strong class=\"fail\">Retransmisión de paquete</strong>"
 		},
 		"fc" : {
-			"fc" : "<strong class=\"fail\">¡Uno o mas mensajes fueron dañados y nadie los entendio!</strong>",
+			"fc" : "<strong class=\"fail\">...BBBZZZ...<br>¡Uno o mas mensajes fueron dañados y nadie los entendio!</strong>",
+		},
+		"http" : {
+			"get" : "Pasame el recurso que esta en esta url:<br><strong>{URL}</strong>",
 		}
 	}
 }
@@ -63,7 +67,7 @@ WiresharkTyrAnalizer.prototype.parse = function(capture) {
 		if (capture[line].indexOf("No.     ") === 0) {
 			if(current_traffic)
 				this.traffic.push(current_traffic);
-			
+
 			next_is_info = true;
 			current_traffic = {
 				"nro" : null,
@@ -98,12 +102,19 @@ WiresharkTyrAnalizer.prototype.parse = function(capture) {
  * Render a HTML table with capture info.
  * @since 1.0
 /*/
-WiresharkTyrAnalizer.prototype.renderTable = function(table, columns, callback) {
+WiresharkTyrAnalizer.prototype.renderTable = function(table, columns, callback, checkboxs) {
 	// Si el parsing no esta listo, esperar hasta que lo este.
 	if(!this.parse_ready) {
-		this.callbacks.parse_ready = this.renderTable.bind(this, table, columns, callback);
+		this.callbacks.parse_ready = this.renderTable.bind(this, table, columns, callback, checkboxs);
 		return;
 	}
+  
+  this.table = table;
+  this.checkboxs = checkboxs;
+	for(var nro=0; nro<this.checkboxs.length; nro++) {
+		this.checkboxs[nro].addEventListener("change", this.updateFilter);
+	}
+  this.updateFilter();
 
 	var thead = document.createElement("thead"),
 		tbody = document.createElement("tbody"),
@@ -120,22 +131,44 @@ WiresharkTyrAnalizer.prototype.renderTable = function(table, columns, callback) 
 		cell.innerHTML = columns[column];
 		row.appendChild(cell);
 	}
-	this.each(0, tbody, columns, callback);	
+
+	this.each(0, tbody, columns, callback);
+}
+
+WiresharkTyrAnalizer.prototype.updateFilter = function() {
+  var rows = this.table.querySelectorAll("tbody tr");
+  for(var nro=0; nro<rows.length; nro++) {
+    rows[nro].classList.add("hide");
+  }
+
+  for(var nro=0; nro<this.checkboxs.length; nro++) {
+    if (!this.checkboxs[nro].id || !this.checkboxs[nro].checked) {
+      continue;
+    }
+
+    var toShow = this.table.querySelectorAll(this.checkboxs[nro].id.split("-").map(function(mac) { return '[data-related*="' + mac + '"]'; }).join(","));
+    for (var nro1=0; nro1<toShow.length; nro1++) {
+      toShow[nro1].classList.remove("hide");
+    }
+  }
 }
 
 WiresharkTyrAnalizer.prototype.each = function(line, tbody, columns, callback) {
 	if(line >= this.traffic.length)
 		return;
-	
+
 	callback || (callback = this.each.bind(this));
-	
+
+	var row = document.createElement("tr"),
+            column, cell;
+  
 	/*/
 	 * Now, parse the information for organization.
 	/*/
 	var current_traffic = {}, prop;
 	for(prop in this.traffic[line])
 		current_traffic[prop] = this.traffic[line][prop];
-	
+
 	var info = current_traffic.other[2].split("(");
 	info.mac_source = info[1].substring(0, info[1].indexOf(")"));
 	info.mac_destination = info[2].substring(0, info[2].indexOf(")"));
@@ -143,126 +176,163 @@ WiresharkTyrAnalizer.prototype.each = function(line, tbody, columns, callback) {
 		case "ARP" :
 			if(current_traffic.destination == "Broadcast") {
 				var split = current_traffic.info.split("?  Tell ");
-				
+
 				current_traffic.source = "<strong>" + split[1] + "</strong>" +
 										"<em>" + info.mac_source + "</em>";
 				current_traffic.info = this.i18n.arp.who_has.replace("{IP}", split[0].substring(8));
-	
+
 				current_traffic.destination = "<strong>Broadcast</strong>" +
 											"<em>" + info.mac_destination + "</em>";
+                      
+        // -----------------------        
+        var broad = document.querySelectorAll('.graph [data-broad*="' + info.mac_source + '"]');        
+        row.dataset.related = ""
+        for (var nro=0; nro<broad.length; nro++) {
+          row.dataset.related += broad[nro].dataset.broad;
+        }
+        // -----------------------        
 			} else {
 				var split = current_traffic.info.split(" is at ");
 				current_traffic.source = "<strong>" + split[0] + "</strong>" +
 										"<em>" + split[1] + "</em>";
-				current_traffic.info = this.i18n.arp.i_has.replace("{MAC}", split[1]);
-	
-				current_traffic.destination = "<em>" + info.mac_destination + "</em>";
+				current_traffic.info = this.i18n.arp.i_has.replace("{IP}", split[0]);
+
+				current_traffic.destination = "<strong><br></strong>" +
+											"<em>" + info.mac_destination + "</em>";
+                      
+        // -----------------------        
+        row.dataset.related = [split[1], info.mac_destination].join("-");
+        // -----------------------        
 			}
 		break;
-		case "TCP" :		
-			
+		case "TCP" :
+
 			if(current_traffic.info.indexOf("[TCP Out-Of-Order]") !== -1) {
-				current_traffic.info = this.i18n.tcp.ooo;				
+				current_traffic.info = this.i18n.tcp.ooo;
 			} else if(current_traffic.info.indexOf("[TCP Retransmission]") !== -1) {
-				current_traffic.info = this.i18n.tcp.retransmission;				
+				current_traffic.info = this.i18n.tcp.retransmission;
 			} else if(current_traffic.info.indexOf("[SYN]") !== -1) {
-				current_traffic.info = this.i18n.tcp.syn.replace("{SOURCE}", current_traffic.source)
-														.replace("{DESTINATION}", current_traffic.destination);
+				current_traffic.info = this.i18n.tcp.syn.replace(/\{SOURCE\}/gi, current_traffic.source)
+														.replace(/\{DESTINATION\}/gi, current_traffic.destination);
 			} else if(current_traffic.info.indexOf("[SYN, ACK]") !== -1) {
-				current_traffic.info = this.i18n.tcp.syn_ack.replace("{SOURCE}", current_traffic.source)
-															.replace("{DESTINATION}", current_traffic.destination);
+				current_traffic.info = this.i18n.tcp.syn_ack.replace(/\{SOURCE\}/gi, current_traffic.source)
+															.replace(/\{DESTINATION\}/gi, current_traffic.destination);
 			}  else if(current_traffic.info.indexOf("[ACK]") !== -1) {
 				current_traffic.info = this.i18n.tcp.ack;
-			} 
+			}
 			current_traffic.source = "<strong>" + current_traffic.source + "</strong>" +
 									"<em>" + info.mac_source + "</em>";
 			current_traffic.destination = "<strong>" + current_traffic.destination + "</strong>" +
 										"<em>" + info.mac_destination + "</em>";
+                    
+      // -----------------------        
+      row.dataset.related = [info.mac_source, info.mac_destination].join("-");
+      // -----------------------    
 		break;
 		case "TLSV1.2" :
 			if(current_traffic.info.indexOf("[TCP Out-Of-Order]") !== -1) {
-				current_traffic.info = this.i18n.tcp.ooo;				
+				current_traffic.info = this.i18n.tcp.ooo;
 			} else if(current_traffic.info.indexOf("[TCP Retransmission]") !== -1) {
-				current_traffic.info = this.i18n.tcp.retransmission;				
+				current_traffic.info = this.i18n.tcp.retransmission;
 			}
 			current_traffic.source = "<strong>" + current_traffic.source + "</strong>" +
 									"<em>" + info.mac_source + "</em>";
 			current_traffic.destination = "<strong>" + current_traffic.destination + "</strong>" +
 										"<em>" + info.mac_destination + "</em>";
+                    
+      // -----------------------        
+      row.dataset.related = [info.mac_source, info.mac_destination].join("-");
+      // -----------------------  
 		break;
-		case "FC" :		
+		case "FC" :
 			current_traffic.info = this.i18n.fc.fc;
 			current_traffic.source = "----";
 			current_traffic.destination = "----";
+      // -----------------------        
+      var broad = document.querySelectorAll('.graph [data-broad]');        
+      row.dataset.related = ""
+      for (var nro=0; nro<broad.length; nro++) {
+        row.dataset.related += broad[nro].dataset.broad;
+      }
+      // -----------------------       
 		break;
-		case "HTTP" :		
+		case "HTTP" :
 			current_traffic.source = "<strong>" + current_traffic.source + "</strong>" +
 									"<em>" + info.mac_source + "</em>";
 			current_traffic.destination = "<strong>" + current_traffic.destination + "</strong>" +
 										"<em>" + info.mac_destination + "</em>";
+
+			if(current_traffic.info.indexOf("GET ") === 0) {
+				current_traffic.info = this.i18n.http.get.replace(/\{URL\}/gi, current_traffic.info.split(" ")[1]);
+			}
+      // -----------------------        
+      row.dataset.related = [info.mac_source, info.mac_destination].join("-");
+      // -----------------------  
 		break;
 	}
-	
-	var row = document.createElement("tr"),
-	column, cell;
-	
+
 	tbody.appendChild(row);
 	for(column in columns) {
 		cell = document.createElement("td");
 		cell.innerHTML = current_traffic[column];
 		row.appendChild(cell);
 	}
-	
+
 	callback(line+1, tbody, columns, null, [current_traffic, this.traffic[line]]);
+  
+  this.updateFilter();
 }
 
 /*/
  * Render a HTML table with capture info.
  * @since 1.0
 /*/
-WiresharkTyrAnalizer.prototype.timelineRender = function(messages, table, columns) {
-	this.renderTable(table, columns, this.render.bind(this, messages));
-	
+WiresharkTyrAnalizer.prototype.timelineRender = function(messages, table, columns, checkboxs) {
+	this.renderTable(table, columns, this.render.bind(this, messages), checkboxs);
 }
+
 WiresharkTyrAnalizer.prototype.render = function (messages, line, tbody, columns, callback, current_traffic) {
 	var elements, nro, edit = current_traffic[0];
 	current_traffic = current_traffic[1];
-	
+
 	elements = document.querySelectorAll('.source');
 	for(nro=0; nro<elements.length; nro++)
 		elements[nro].classList.remove("source");
-	
+
 	elements = document.querySelectorAll('.destination');
 	for(nro=0; nro<elements.length; nro++)
 		elements[nro].classList.remove("destination");
-			
+
 	var info = current_traffic.other[2].split("(");
 	info.mac_source = info[1].substring(0, info[1].indexOf(")"));
 	info.mac_destination = info[2].substring(0, info[2].indexOf(")"));
-		
+
 	elements = document.querySelectorAll('div[data-macs*="' + info.mac_destination + '"]');
 	for(nro=0; nro<elements.length; nro++)
 		elements[nro].classList.add("destination");
-	
+
 	elements = document.querySelectorAll('div[data-macs*="' + info.mac_source + '"]');
 	for(nro=0; nro<elements.length; nro++)
 		elements[nro].classList.add("source");
-	
+
 	if(current_traffic.destination == "Broadcast") {
 		var broad;
 		for(nro=0; nro<elements.length; nro++)
 		{
 			broad = document.querySelectorAll('.red[data-broad*="' + info.mac_source + '"] div.node:not([data-macs*="' + info.mac_source + '"]), .red[data-broad*="' + info.mac_source + '"] div.cloud');
 			for(nro=0; nro<broad.length; nro++)
-				broad[nro].classList.add("destination");			
-		}			
-	}	
-	
+				broad[nro].classList.add("destination");
+		}
+	}
+
 	messages.querySelector('.mnro').innerHTML = edit.nro;
 	messages.querySelector('.msource').innerHTML = edit.source;
 	messages.querySelector('.mdestination').innerHTML = edit.destination;
 	messages.querySelector('.mprotocol').innerHTML = edit.protocol;
 	messages.querySelector('.minfo').innerHTML = current_traffic.info;
-	
+	messages.querySelector('.mexplanation').innerHTML = edit.info;
+
 	setTimeout(this.each.bind(this, line, tbody, columns, this.render.bind(this, messages)), this.animation_delay);
+  
+  this.updateFilter();
 };
